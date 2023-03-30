6d8e14323c8c30437a193e646b831ae5030d77fa
panicdragon <d.takayama.web@gmail.com>
2023-03-31 00:47:13 +0900

    feat: 🎸 add tile map func

--- lib/game.dart
+++ lib/game.dart
@@ -5,7 +5,7 @@ import 'package:flame/input.dart';
 import 'package:tiled/tiled.dart';
 
 class MainGame extends FlameGame with KeyboardEvents, HasGameRef {
-  static const scale = 2.0;
+  static const scale = 1.0;
   static const srcTileSize = 64.0;
   static const destTileSize = scale * srcTileSize;
 
@@ -14,13 +14,35 @@ class MainGame extends FlameGame with KeyboardEvents, HasGameRef {
 
   late IsometricTileMapComponent base;
 
+  final topLeft = Vector2(600, 0);
+
   @override
   Future<void> onLoad() async {
     super.onLoad();
 
     final tmxData = await assets.readFile('tiles/tile_map.tmx');
     final tmx = TileMapParser.parseTmx(tmxData);
-    final matrix = List<List<int>>.generate(tmx.height, (y) => List<int>.filled(tmx.width, 0));
+    // final tilesets = tmx.tilesets;
+    final layers = tmx.layers.whereType<TileLayer>();
+    // final tileSize = tmx.tileWidth.toDouble();
+    final mapWidth = layers.first.width;
+    final mapHeight = layers.first.height;
+
+    final data = layers.first.data;
+
+    final matrix = List<List<int>>.generate(mapHeight, (row) => List.generate(mapWidth, (col) => data![row * mapWidth + col]),);
+
+    // for (final layer in tmx.layers) {
+    //   if (layer is TileLayer) {
+    //     for (var y = 0; y < tmx.height; y++) {
+    //       for (var x = 0; x < tmx.width; x++) {
+    //         final index = layer.tiles[x + y * tmx.width].tileId;
+    //         matrix[y][x] = index;
+    //       }
+    //     }
+    //   }
+    // }
+
     final tilesetImage = await images.load('tile_map.png');
     final tileset = SpriteSheet(
       image: tilesetImage,
@@ -33,7 +55,7 @@ class MainGame extends FlameGame with KeyboardEvents, HasGameRef {
         matrix,
         destTileSize: Vector2.all(destTileSize),
         tileHeight: tileHeight,
-        position: Vector2(700, 0),
+        position: topLeft,
       ),
     );
   }
